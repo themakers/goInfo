9121af445b612e3c3ce7c6d6b624b9e16ea18265
The Architect <thearchitect@themake.rs>
2017-10-27 18:12:41 +0400

    General refactoring; Added os-release support on linux;  More informative on windows (github.com/ss23/goInfo)

--- goInfo.go
+++ goInfo.go
@@ -1,29 +1,119 @@
-package goInfo
+package osinfo
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"regexp"
+	"strings"
 )
 
-type GoInfoObject struct {
-	GoOS string
-	Kernel string
-	Core string
+type OSInfo struct {
+	GoOS     string
+	Kernel   string
+	Core     string
 	Platform string
-	OS string
+	OS       string
 	Hostname string
-	CPUs int
+	CPUs     int
+
+	LinuxRelease *LinuxOSRelease
 }
 
-func (gi *GoInfoObject) VarDump() {
-	fmt.Println("GoOS:",gi.GoOS)
-	fmt.Println("Kernel:",gi.Kernel)
-	fmt.Println("Core:",gi.Core)
-	fmt.Println("Platform:",gi.Platform)
-	fmt.Println("OS:",gi.OS)
-	fmt.Println("Hostname:",gi.Hostname)
-	fmt.Println("CPUs:",gi.CPUs)
+func (gi *OSInfo) AsJSON() string {
+	if data, err := json.MarshalIndent(gi, "", " "); err != nil {
+		panic(err)
+	} else {
+		return string(data)
+	}
 }
 
-func (gi *GoInfoObject) String() string {
-	return fmt.Sprintf("GoOS:%v,Kernel:%v,Core:%v,Platform:%v,OS:%v,Hostname:%v,CPUs:%v",gi.GoOS,gi.Kernel,gi.Core,gi.Platform,gi.OS,gi.Hostname,gi.CPUs)
+// func (gi *OSInfo) String() string {
+// 	return fmt.Sprintf("GoOS:%v,Kernel:%v,Core:%v,Platform:%v,OS:%v,Hostname:%v,CPUs:%v", gi.GoOS, gi.Kernel, gi.Core, gi.Platform, gi.OS, gi.Hostname, gi.CPUs)
+// }
+
+type LinuxOSRelease struct {
+	Name             string
+	Version          string
+	ID               string
+	IDLike           []string
+	VersionCodename  string
+	VersionID        string
+	PrettyName       string
+	ANSIColor        string
+	CPEName          string
+	HomeURL          string
+	SupportURL       string
+	BugReportURL     string
+	PrivacyPolicyURL string
+	BuildID          string
+	Variant          string
+	VariantID        string
+
+	ExtendedFields map[string]string
+}
+
+// https://www.freedesktop.org/software/systemd/man/os-release.html
+func newLinuxOSRelease() (*LinuxOSRelease, error) {
+	osrData, err := (func(paths ...string) (data []byte, err error) {
+		for _, path := range paths {
+			data, err = ioutil.ReadFile(path)
+			if err == nil {
+				break
+			}
+		}
+		return
+	})(
+		"/etc/os-release",
+		"/usr/lib/os-release",
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	osr := &LinuxOSRelease{}
+
+	for _, mch := range regexp.MustCompile(`([A-Za-z0-9\_]+)=(.*)\n`).FindAllStringSubmatch(string(osrData), -1) {
+		name, val := mch[1], strings.Trim(mch[2], " \"")
+		switch name {
+		case "NAME":
+			osr.Name = val
+		case "VERSION":
+			osr.Version = val
+		case "ID":
+			osr.ID = val
+		case "ID_LIKE":
+			osr.IDLike = strings.Split(val, " ")
+		case "VERSION_CODENAME":
+			osr.VersionCodename = val
+		case "VERSION_ID":
+			osr.VersionID = val
+		case "PRETTY_NAME":
+			osr.PrettyName = val
+		case "ANSI_COLOR":
+			osr.ANSIColor = val
+		case "CPE_NAME":
+			osr.CPEName = val
+		case "HOME_URL":
+			osr.HomeURL = val
+		case "SUPPORT_URL":
+			osr.SupportURL = val
+		case "BUG_REPORT_URL":
+			osr.BugReportURL = val
+		case "PRIVACY_POLICY_URL":
+			osr.PrivacyPolicyURL = val
+		case "BUILD_ID":
+			osr.BuildID = val
+		case "VARIANT":
+			osr.Variant = val
+		case "VARIANT_ID":
+			osr.VariantID = val
+		default:
+			if osr.ExtendedFields == nil {
+				osr.ExtendedFields = make(map[string]string)
+			}
+			osr.ExtendedFields[name] = val
+		}
+	}
+
+	return osr, nil
 }
